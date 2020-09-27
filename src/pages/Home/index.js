f6adb7247f0b8e3bcf4c812457e9ef86d3b05dfe
xuguanjie <xuguanjie@halove.com>
2020-09-27 11:03:11 +0800

    bug

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 const Home = props => {
-  // console.log(props);
+  console.log(props);
   const {
     history,
     location: { query },
   } = props;
   console.log(query);
-  return <div>{JSON.stringify(query)}</div>;
+  return (
+    <div>
+      {JSON.stringify(query)}1233{window.location.href}
+    </div>
+  );
 };
 
 export default Home;
