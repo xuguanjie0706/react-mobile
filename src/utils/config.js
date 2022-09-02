366b329492c21f040f9c78dd315908bb1752b099
xuguanjie <xuguanjie@halove.com>
2022-09-03 07:07:43 +0800

    daimatijiao

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: xgj
  * @since: 2022-09-01 21:31:45
- * @lastTime: 2022-09-01 21:41:22
+ * @lastTime: 2022-09-03 07:07:27
  * @LastAuthor: xgj
  * @FilePath: /react-mobile/src/utils/config.js
  * @message:
@@ -10,6 +10,6 @@ export default {
   url:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:7001/'
-      : 'http://pick.yystart.com/',
+      : 'http://124.222.170.81/',
   // url: 'http://pick.yystart.com/',
 };
